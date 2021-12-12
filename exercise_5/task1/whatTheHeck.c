682586fdc440743d0a9be3890dca711852686233
Tiim <tim.bachmann96@gmail.com>
2021-12-12 16:48:57 +0100

    Add explanation for "whatTheHeck" as stated in the ex. sheet

--- exercise_5/task1/whatTheHeck.c
+++ exercise_5/task1/whatTheHeck.c
@@ -7,6 +7,14 @@ int sum(const int* arr, size_t n) {
   omp_set_num_threads(15);
   #pragma omp parallel for
   for(size_t i=0; i < n; ++i) {
+    
+    // We modify the same value s from multiple threads.
+    // Therefore it is possible that some modifications happen at the same
+    // time which leads to missed modifications, and therefore a sum which is too
+    // low.
+    // using the atomic pragma we tell the compiler to introduce safeguards arount
+    // the statement which makes the assignement atomic.
+    
     #pragma omp atomic
     s += arr[i];
   }
