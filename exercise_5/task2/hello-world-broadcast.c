b2c9a098105e973d915c337cd1f914b19adc5744
Tiim <tim.bachmann96@gmail.com>
2021-12-12 14:06:23 +0100

    Add broadcast t1

---
+++ exercise_5/task2/hello-world-broadcast.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mpi.h"
+#include <omp.h>
+
+void send_chars(int rank, int tasks) {
+    char buffer[50];
+    MPI_Status status;
+    MPI_Request rq;
+
+    if (rank == 0) {
+        strcpy(buffer, "hello world");
+    }
+    MPI_Bcast(buffer, 50, MPI_CHAR, 0, MPI_COMM_WORLD);
+    for (int i = 1; i < tasks; i++) {
+        MPI_Bcast(buffer, 50, MPI_CHAR, i, MPI_COMM_WORLD);
+        if (i == tasks - 1 && rank == tasks - 1) {
+            printf("RECEIVED BROADCAST %s\n", buffer);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int npes, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    //printf("From process %d out of %d!\n", rank, npes);
+
+    send_chars(rank, npes);
+
+    //printf("DONE! (rank: %d)\n", rank);
+
+    MPI_Finalize();
+    return 0;
+}
+
+
+
---
+++ exercise_5/task2/job-broadcast.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J job1
+#SBATCH --time=00:05:00
+#SBATCH --exclusive
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=xeon
+#SBATCH --output=OUTPUT.txt
+
+
+module load intel
+mpiicc -O3 hello-world-broadcast.c -o hw
+srun hw
